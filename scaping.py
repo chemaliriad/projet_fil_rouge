64464a22f538d32c82599995ad79e84c28044411
unknown <riadaerty@gmail.com>
2023-05-25 14:16:29 +0200

    commit2

--- scaping.py
+++ scaping.py
@@ -1,5 +1,14 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html)
+        s
+for sibling in bsObj.find("table",{"id":"giftList"}).tr.next_siblings:
+            print(sibling)
+
+        
+        
+        
+
